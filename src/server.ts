5691421ea5f86552af839b7b0dce87c09eb06fd9
SajidSojib <sajidahmed2401@gmail.com>
2025-11-26 20:28:30 +0600

    post method in nodejs

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,42 @@ const server:Server = http.createServer((req:IncomingMessage, res:ServerResponse
 
     if(req.url === '/' && req.method === 'GET') {
         res.writeHead(200, {'content-type': 'application/json'});
-        res.end(JSON.stringify({message: 'Hello from nodejs with typescript', port: req.url}));
+        res.end(JSON.stringify({message: 'Hello from nodejs with typescript', path: req.url}));
+    }
+
+    if(req.url === '/api' && req.method === 'GET') {
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(JSON.stringify({message: 'health status ok', path: req.url}));
+    }
+
+    if(req.url === '/api/users' && req.method === 'POST') {
+      // const user = {
+      //   name: 'John Doe',
+      //   age: 30,
+      // }
+      // res.writeHead(200, {'content-type': 'application/json'});
+      // res.end(JSON.stringify(user));
+
+      let body = "";                            //? must be sting
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {                     //? only call after all chunk loads
+        try {
+          //   res.end(body)                       //? sends body
+
+          const perseBody = JSON.parse(body);
+          // res.end(perseBody)                    //! error
+          res.end(JSON.stringify(perseBody));
+          
+        } catch (err:any) {
+          res.statusCode = 400;
+          res.end(err?.message);
+        }
+      });
+
+        // res.end(body)   //? empty
     }
 })
 
