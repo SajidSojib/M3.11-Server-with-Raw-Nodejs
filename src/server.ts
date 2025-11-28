18e8e860a3c6c7fb7bdcb1f760c4f2aea90682f2
SajidSojib <sajidahmed2401@gmail.com>
2025-11-28 20:23:55 +0600

    created dinamic route handler

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,29 @@ import config from "./config";
 import { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes";
 
+function findDinamicRoute(method: string, url: string) {
+    const methodMap = routes.get(method);
+    if(!methodMap) return undefined; //? return undefined if not found (method)
+    
+    for(const [path, handler] of methodMap) {
+        const routeParts = path.split('/');
+        const urlParts = url.split('/');
+        if(routeParts.length !== urlParts.length) continue;
+        let params:any = {};
+        let match = true;
+        for(let i = 0; i < routeParts.length; i++) {
+            if(routeParts[i]?.startsWith(':')) {
+                params[routeParts[i]?.substring(1)!] = urlParts[i];
+            }else if(routeParts[i] !== urlParts[i]) {
+                match = false;
+                break;
+            }
+        }
+        if(match) {
+            return {handler, params};
+        }
+    }
+}
 
 const server:Server = http.createServer((req:IncomingMessage, res:ServerResponse) => {
     console.log('server running...');
@@ -13,6 +36,10 @@ const server:Server = http.createServer((req:IncomingMessage, res:ServerResponse
     const handler:RouteHandler | undefined = methodMap?.get(path);
 
     if(handler) {
+        handler(req, res);
+    }else if(findDinamicRoute(method, path)) {
+        const {handler, params} = findDinamicRoute(method, path)!;
+        (req as any).params = params;
         handler(req, res);
     }else {
         res.writeHead(404, {'content-type': 'application/json'});
